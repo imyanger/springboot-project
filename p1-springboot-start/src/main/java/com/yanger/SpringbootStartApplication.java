9e710f75a3f4afb2855e945c8f46b97678f8aeb9
github-yanger <550799932@qq.com>
2019-06-05 14:09:58 +0800

    start

---
+++ p1-springboot-start/src/main/java/com/yanger/SpringbootStartApplication.java
@@ -0,0 +1,13 @@
+package com.yanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStartApplication.class, args);
+    }
+
+}
