f33dbfb4f3f7309ce1f6e7da99e0b4ce24226dbd
github-yanger <550799932@qq.com>
2019-06-12 18:57:05 +0800

    jpa

--- p5-springboot-jdbcTemplate/src/main/java/com/yanger/jdbcTemplate/JdbcTemplateDao.java
+++
@@ -1,4 +0,0 @@
-package com.yanger.jdbcTemplate;
-
-public class JdbcTemplateDao {
-}
---
+++ p6-springboot-jpa/src/main/java/com/yanger/jdbcTemplate/UserApi.java
@@ -0,0 +1,65 @@
+package com.yanger.jdbcTemplate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("jdbc/user")
+public class UserApi {
+
+    @Autowired
+    private JUserService userService;
+
+    /**
+     * 添加用户
+     * @param user
+     */
+    @PostMapping("addUser")
+    public String addUser(User user){
+        userService.addUser(user);
+        return "添加用户成功";
+    }
+
+    /**
+     * 删除用户
+     * @param id
+     */
+    @GetMapping("delUser")
+    public String delUser(@RequestParam(value = "id") int id){
+        userService.delUser(id);
+        return "删除用户成功";
+    }
+
+    /**
+     * 修改用户信息
+     * @param user
+     */
+    @PostMapping("updateUser")
+    public String updateUser(User user){
+        userService.updateUser(user);
+        return "修改用户成功";
+    }
+
+    /**
+     * 获取用户信息
+     * @param id
+     * @return
+     */
+    @GetMapping("getUser")
+    public User getUser(@RequestParam(value = "id") int id){
+        return userService.getUser(id);
+    }
+
+    /**
+     * 获取所有用户信息
+     * @return
+     */
+    @GetMapping("getUsers")
+    public List<User> getUsers(){
+        List<User> users = userService.findAll();
+        return users;
+    }
+
+}
