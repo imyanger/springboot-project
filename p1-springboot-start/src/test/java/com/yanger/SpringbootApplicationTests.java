c7671e58cfa9781e144914b08beed7d192c6147b
github-yanger <550799932@qq.com>
2019-06-14 21:15:14 +0800

    test

--- p1-springboot-start/src/test/java/com/yanger/SpringbootApplicationTests.java
+++ p1-springboot-start/src/test/java/com/yanger/SpringbootApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class P1SpringbootStartApplicationTests {
+public class SpringbootApplicationTests {
 
     @Test
     public void contextLoads() {
